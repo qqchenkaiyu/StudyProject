c047207ede6a940d43aa048157c59656edb4efbb
qqchenkaiyu <37261937+qqchenkaiyu@users.noreply.github.com>
2021-12-23 15:56:22 +0800

    Create GPAfterReturningAdviceInterceptor.java

---
+++ src/main/java/Spring/GPAfterReturningAdviceInterceptor.java
@@ -0,0 +1,31 @@
+package com.study.Spring.aop.gpaop;
+
+
+import com.study.Spring.aop.gpaop.intercept.GPMethodInterceptor;
+import com.study.Spring.aop.gpaop.intercept.GPMethodInvocation;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by Tom on 2019/4/15.
+ */
+public class GPAfterReturningAdviceInterceptor extends GPAbstractAspectAdvice implements GPAdvice, GPMethodInterceptor {
+
+    private GPJoinPoint joinPoint;
+
+    public GPAfterReturningAdviceInterceptor(Method aspectMethod, Object aspectTarget) {
+        super(aspectMethod, aspectTarget);
+    }
+
+    @Override
+    public Object invoke(GPMethodInvocation mi) throws Throwable {
+        Object retVal = mi.proceed();
+        this.joinPoint = mi;
+        this.afterReturning(retVal,mi.getMethod(),mi.getArguments(),mi.getThis());
+        return retVal;
+    }
+
+    private void afterReturning(Object retVal, Method method, Object[] arguments, Object aThis) throws Throwable {
+        super.invokeAdviceMethod(this.joinPoint,retVal,null);
+    }
+}
