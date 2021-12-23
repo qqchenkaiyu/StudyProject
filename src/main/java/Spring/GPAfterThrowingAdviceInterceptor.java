1e59fd3fa1aa6ffc752aa3e1bfb95ab076878f56
qqchenkaiyu <37261937+qqchenkaiyu@users.noreply.github.com>
2021-12-23 15:56:07 +0800

    Create GPAfterThrowingAdviceInterceptor.java

---
+++ src/main/java/Spring/GPAfterThrowingAdviceInterceptor.java
@@ -0,0 +1,35 @@
+package com.study.Spring.aop.gpaop;
+
+
+
+import com.study.Spring.aop.gpaop.intercept.GPMethodInterceptor;
+import com.study.Spring.aop.gpaop.intercept.GPMethodInvocation;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by Tom on 2019/4/15.
+ */
+public class GPAfterThrowingAdviceInterceptor extends GPAbstractAspectAdvice implements GPAdvice, GPMethodInterceptor {
+
+
+    private String throwingName;
+
+    public GPAfterThrowingAdviceInterceptor(Method aspectMethod, Object aspectTarget) {
+        super(aspectMethod, aspectTarget);
+    }
+
+    @Override
+    public Object invoke(GPMethodInvocation mi) throws Throwable {
+        try {
+            return mi.proceed();
+        }catch (Throwable e){
+            invokeAdviceMethod(mi,null,e.getCause());
+            throw e;
+        }
+    }
+
+    public void setThrowName(String throwName){
+        this.throwingName = throwName;
+    }
+}
